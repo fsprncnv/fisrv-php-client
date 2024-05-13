6911cae65c1b4e0148aa7ab8dfc3095addb39108
prncn <princen@outlook.de>
2024-05-13 10:34:47 +0200

    Fix InformationLookup marked as todo

--- lib/Payments/InformationLookup.php
+++ lib/Payments/InformationLookup.php
@@ -2,20 +2,20 @@
 
 namespace Fiserv\Payments;
 
-use Fiserv\HttpClient;
 
 class InformationLookup
 {
+    /**
+     * @todo Wrap API method for card lookup
+     */
     public static function cardInfoLookup($client, $requestBody)
     {
-        $endpoint = '/ipp/payments-gateway/v2/card-information';
-        return HttpClient::buildRequest($client, 'POST', $endpoint, $requestBody);
     }
 
+    /**
+     * @todo Wrap API method for account lookup
+     */
     public static function lookUpAccount($client, $cardNumber)
     {
-        $requestBody = new \PaymentToken('1235325235236', '12345500001', 'CREDIT', '977', new \ExpiryDate('02', '28'));
-        $endpoint = '/ipp/payments-gateway/v2/account-information';
-        return HttpClient::buildRequest($client, 'POST', $endpoint, $requestBody);
     }
 }
