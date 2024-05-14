b65fc0d90ce4dbcab0171f7dd5d7e8c24c3f9f4a
prncn <princen@outlook.de>
2024-05-14 14:01:34 +0200

    Add template method for SEPA checkout

--- lib/Checkout/CheckoutSolution.php
+++ lib/Checkout/CheckoutSolution.php
@@ -15,7 +15,7 @@ class CheckoutSolution
      * Create a checkout link to be used as checkout solution.
      * Pass an optional webhook to receive status events from fiserv server.
      * 
-     * @var PaymentLinkRequestBody $req - Request body containing checkout options
+     * @param PaymentLinkRequestBody $req - Request body containing checkout options
      */
     public static function postCheckouts(PaymentLinkRequestBody $req): PostCheckoutsResponse
     {
@@ -26,12 +26,34 @@ class CheckoutSolution
         return $data;
     }
 
+    /**
+     * Create a checkout link that uses default parameters for SEPA payment.
+     * 
+     * @todo Currently, the request object is inaccessible in this kind of function call.
+     * Possibly create a callback option or simply return an array containing the response (like now)
+     * and request data, both. 
+     * 
+     * @see CheckoutSolution::postCheckouts
+     * @param float $transactionTotal Total transaction amount (in EUR)
+     * @param string $successUrl URL that directs to Thank You page from checkout
+     * @param string $failureUrl URL that directs to failure notifaction if checkout failed
+     */
+    public static function createSEPACheckout(float $transactionTotal, string $successUrl, string $failureUrl): PostCheckoutsResponse
+    {
+        $req = new PaymentLinkRequestBody(Fixtures::paymentLinksRequestContent);
+        $req->transactionAmount->total = $transactionTotal;
+        $req->checkoutSettings->redirectBackUrls->successUrl = $successUrl;
+        $req->checkoutSettings->redirectBackUrls->failureUrl = $failureUrl;
+
+        return self::postCheckouts($req);
+    }
+
 
     /**
      * Query an existing checkout link object by given ID. 
      * Responds with verbose list about checkout configuration.
      * 
-     * @var string $checkoutId - String checkout ID to be queried  
+     * @param string $checkoutId - String checkout ID to be queried  
      */
     public static function getCheckoutId(string $checkoutId): GetCheckoutIdResponse
     {
