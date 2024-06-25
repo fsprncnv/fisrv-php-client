4276fa8b504b1be57e975045e636f822e0482da2
Marc Herrmann <info@marcherrmann.com>
2024-06-25 08:14:47 +0000

    Update .php-cs-fixer.php

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,7 +1,11 @@
 <?php
 
-return (new PhpCsFixer\Config)
-    ->setRules([
-        '@Symfony' => true,
-        'array_syntax' => ['syntax' => 'short'],
-    ]);
+$finder = Symfony\CS\Finder\DefaultFinder::create()
+    ->exclude('vendor')
+    ->in(__DIR__)
+;
+
+return Symfony\CS\Config\Config::create()
+    ->fixers(array('indentation', 'elseif'))
+    ->finder($finder)
+;
