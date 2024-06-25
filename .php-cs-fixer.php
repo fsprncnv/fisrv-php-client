775460af3e7b749937c7b508e8e3d4f54295bb67
Marc Herrmann <info@marcherrmann.com>
2024-06-25 08:09:21 +0000

    Update .php-cs-fixer.php

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -4,6 +4,5 @@ $finder = Symfony\CS\Finder\DefaultFinder::create()
     ->in('src');
 
 return Symfony\CS\Config\Config::create()
-    ->level(Symfony\CS\FixerInterface::SYMFONY_LEVEL)
     ->fixers(['-yoda_conditions', 'short_array_syntax', '-phpdoc_align'])
     ->finder($finder);
