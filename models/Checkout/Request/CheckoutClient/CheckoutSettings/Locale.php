513a4100f45bde53c1b5e1411ba9d25371b25c96
prncn <princen@outlook.de>
2024-09-10 15:54:34 +0200

    Add locales and add composer run-all script

--- models/Checkout/Request/CheckoutClient/CheckoutSettings/Locale.php
+++ models/Checkout/Request/CheckoutClient/CheckoutSettings/Locale.php
@@ -7,6 +7,7 @@ enum Locale: string
     case en_GB = 'en_GB';
     case en_US = 'en_US';
     case de_DE = 'de_DE';
-    case de_PL = 'de_PL';
     case nl_NL = 'nl_NL';
+    case es_ES = 'es_ES';
+    case fr_FR = 'fr_FR';
 }
